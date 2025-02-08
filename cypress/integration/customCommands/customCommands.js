745c56f9cae7aa32ba04e576ae3fa8a1b11585fe
a2sat <saadua207@gmail.com>
2025-02-08 10:56:08 +0000

    initial commit

---
+++ cypress/integration/customCommands/customCommands.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />​
+
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+beforeEach( function(){
+cy.fixture('example').then(function(data)
+{
+this.data = data
+})
+})
+
+Given ('I navigate to demoblaze website', function() {
+cy.visit(Cypress.env("baseURL"));
+cy.viewport(1800,1000)
+})
+
+When ('I click the login button', function(){
+cy.get('#login2').click()
+cy.wait(2000)
+})
+
+When ('I enter valid login details', function(){
+cy.get('#loginusername').type(this.data.Username)
+//cy.get('#loginusername').clear()
+cy.get('#loginpassword').type(this.data.Password)
+cy.get("button[onclick='logIn()']").click()
+})
